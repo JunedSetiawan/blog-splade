5fbb5e0d6c57e3ebe44eaf63f4af64c3109360f4
JunedSetiawan <junedhayko502@gmail.com>
2023-08-23 10:52:04 +0700

    make permission,role & policy

---
+++ app/Models/Role.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role as modelRole;
+
+class Role extends modelRole
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'guard_name',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'string',
+    ];
+
+    /**
+     *  The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+}
---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Permission;
+use App\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Make Permission for Users
+        Permission::create(['name' => 'create-users']);
+        Permission::create(['name' => 'edit-users']);
+        Permission::create(['name' => 'delete-users']);
+
+        //Make Permission for Posts
+        Permission::create(['name' => 'create-posts']);
+        Permission::create(['name' => 'edit-posts']);
+        Permission::create(['name' => 'delete-posts']);
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $writerRole = Role::create(['name' => 'writer']);
+
+        $adminRole->givePermissionTo([
+            'create-users',
+            'edit-users',
+            'delete-users',
+            'create-posts',
+            'edit-posts',
+            'delete-posts'
+        ]);
+
+        $writerRole->givePermissionTo([
+            'create-posts',
+            'edit-posts',
+            'delete-posts'
+        ]);
+
+        $user = User::first();
+        $user->assignRole($adminRole);
+    }
+}
